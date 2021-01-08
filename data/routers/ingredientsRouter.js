aec8fa17fed56cdf8e0c02360aa25072ffd55ccc
Dodger-42 <kevin.bing.barber@gmail.com>
2021-01-07 17:14:56 -0800

    working on endpoints

--- data/routers/ingredientsRouter.js
+++ data/routers/ingredientsRouter.js
@@ -1,5 +1,6 @@
 express = require("express")
 const db = require("../config")
+
 const router = express.Router()
 
 // router.get("/", async (req, res, next)=> {
@@ -9,6 +10,44 @@ const router = express.Router()
 //     catch(err){
 //         next(err)
 //     }
+// })
+
+
+
+
+//--- from the CLASS PROJECT  -------
+
+// router.get("/zoos", async (req, res, next) => {
+// 	try {
+// 		const zoos = await Zoo.find()
+// 		res.json(zoos)
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.get("/zoos/:id", async (req, res, next) => {
+// 	try {
+// 		const zoo = await Zoo.findById(req.params.id)
+// 		if (!zoo) {
+// 			return res.status(404).json({
+// 				message: "Zoo not found",
+// 			})
+// 		}
+
+// 		res.json(zoo)
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.get("/zoos/:id/animals", async (req, res, next) => {
+// 	try {
+// 		const animals = await Zoo.findAnimals(req.params.id)
+// 		res.json(animals)
+// 	} catch (err) {
+// 		next(err)
+// 	}
 // })
 
 module.exports = router
--- data/routers/recipeRouter.js
+++ data/routers/recipeRouter.js
@@ -1,5 +1,6 @@
 express = require("express")
 const db = require("../config")
+
 const router = express.Router()
 
 // router.get("/", async (req, res, next)=> {
@@ -10,5 +11,37 @@ const router = express.Router()
 //         next(err)
 //     }
 // })
+
+
+
+
+
+// ------  FROM THE CLASS PROJECT   --------
+
+// router.get("/animals", async (req, res, next) => {
+// 	try {
+// 		const animals = await Animal.find()
+// 		res.json(animals)
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+// router.get("/animals/:id", async (req, res, next) => {
+// 	try {
+// 		const animal = await Animal.findById(req.params.id)
+// 		if (!animal) {
+// 			return res.status(404).json({
+// 				message: "Animal not found",
+// 			})
+// 		}
+
+// 		res.json(animal)
+// 	} catch(err) {
+// 		next(err)
+// 	}
+// })
+
+
 
 module.exports = router
